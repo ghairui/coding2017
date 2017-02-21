7ea2ffbcec9356c3618fe824d135aa9c14904ea8
ghairui <ghairui@gmail.com>
2017-02-21 16:41:59 +0800

    git测试初始化

---
+++ group09/85839593/assignment-0215-0226/src/test/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: guohairui
+ * Date: 17-2-21
+ * Time: 下午4:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String [] g){
+                 System.out.print("hello,world");
+    }
+}
